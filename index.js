2af0e35734e40870a75d239b0858ce46908e4c96
anhcong123c <dalesarvernam1311@outlook.com>
2024-05-10 14:24:24 +0700

    demo task

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const remove = (arr, func) =>
-  Array.isArray(arr)
-    ? arr.filter(func).reduce((acc, val) => {
-        arr.splice(arr.indexOf(val), 1);
-        return acc.concat(val);
-      }, [])
-    : [];
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+  return count.every((c) => c === 0);
+}
